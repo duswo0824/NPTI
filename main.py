fa575b858a68e47460e789cfbf75c26e3965088c
aidus011022 <suzie011022@naver.com>
2025-12-30 10:00:05 +0900

    main.py(수정중) & requirements.txt 테스트용 커밋

---
+++ main.py
@@ -0,0 +1,90 @@
+import hashlib
+import pandas as pd
+from fastapi import FastAPI
+from selenium.webdriver.chromium.options import ChromiumOptions
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from bigkinds_crawling.scheduler import sch_start
+
+from bigkinds_crawling.sample import sample_crawling, get_sample
+from logger import Logger
+import time
+from typing import Optional
+from elasticsearch_index.es_raw import ensure_news_raw, index_sample_row, search_news_row, tokens
+from elasticsearch_index.es_raw import es, ES_INDEX
+from kiwipiepy import Kiwi
+from bigkinds_crawling.news_raw import news_crawling, get_news_raw, news_aggr
+
+app = FastAPI()
+logger = Logger().get_logger(__name__)
+
+
+@app.get("/sample")
+def sample(max_pages: int = 90):
+    logger.info(f"API 호출: 크롤링 시작 (최대 {max_pages} 페이지)")
+    try:
+        # 비즈니스 로직 호출
+        result = sample_crawling(max_pages=max_pages)
+        return {"status": "success","count": len(result),"data": result}
+    except Exception as e:
+        logger.error(f"API 실행 오류: {e}")
+        return {"status": "error", "message": str(e)}
+
+
+@app.get("/sample_csv")
+def sample_csv(q: Optional[str] = None):
+    logger.info(f"ES 데이터 요청 수신 (query: {q})")
+    try:
+        result = get_sample(q)
+        if result is None:
+            return {"status": "error", "message": "데이터를 가져올 수 없습니다."}
+        return result
+    except Exception as e:
+        logger.error(f"API 실행 오류: {e}")
+        return {"status": "error", "message": str(e)}
+
+
+@app.get("/news_raw")
+def news_raw(max_pages: int = 5):
+    logger.info(f"크롤링 시작: 최대 {max_pages} 페이지")
+    try:
+        # sample.py의 crawling 함수 호출
+        result = news_crawling(max_pages=max_pages)
+        return {"status": "success","count": len(result),"data": result}
+    except Exception as e:
+        logger.error(f"API 실행 중 오류 발생: {e}")
+        return {"status": "error", "message": str(e)}
+
+sch = sch_start()
+@app.get("/scheduler_start")
+async def scheduler_start():
+    if not sch.running:
+        sch.start()
+        return {'msg': 'scheduler 실행 시작!'}
+    else:
+        return {'msg': '이미 실행 중입니다.'}
+
+@app.get("/news_aggr")
+def news_aggr_start():
+    tfid = news_aggr()
+    return tfid
+
+
+@app.get("/news_raw_csv")
+def news_raw_csv(q: Optional[str] = None):
+    logger.info(f"ES 데이터 조회 요청: query={q}")
+    try:
+        news_list = get_news_raw(q)
+        if news_list is None:
+            return {"status": "error", "message": "데이터를 가져올 수 없습니다."}
+        return news_list
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
