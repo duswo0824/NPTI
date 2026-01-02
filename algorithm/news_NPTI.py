4cbd7971f05dd3665a597954b28cea7773a05194
tannookim <tannookim@gmail.com>
2026-01-02 17:02:33 +0900

    기사 NPTI 분류 알고리즘 생성

--- algorithm/news_NPTI.py
+++ algorithm/news_NPTI.py
@@ -1,5 +1,9 @@
 from logger import Logger
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, helpers
+import pymysql
+from sqlalchemy import Column, String, DateTime, ForeignKey
+from sqlalchemy.sql import func
+from database import Base, get_engine, SessionLocal
 
 logger = Logger().get_logger(__name__)
 
@@ -15,42 +19,90 @@ es = Elasticsearch( # elasticsearch 연결 객체 생성
     ssl_show_warn=False # type: ignore
 )
 
+# articles_NPTI 테이블 정의
+class ArticlesNPTI(Base):
+    __tablename__ = 'articles_NPTI'
+
+    news_id = Column(String(100), primary_key=True)
+    NPTI_code = Column(String(50), ForeignKey('npti_code.NPTI_code'))
+    length_type = Column(String(10))  # L/S
+    article_type = Column(String(10))  # T/C
+    info_type = Column(String(10))  # F/I
+    view_type = Column(String(10))  # P/N
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
+
+# 테이블 생성 함수
+def add_db():
+    engine = get_engine()
+    Base.metadata.create_all(bind=engine, tables=[ArticlesNPTI.__table__])
 
 # 1. length type 분류
-def classify_length(news_id:str):
+def classify_length():
+    add_db()
+    db = SessionLocal()
     try:
-        res = es.get(
-            index=ES_INDEX,
-            id=news_id,
-            _source=["content"],
-        )
-        content = res['_source'].get('content','')
-        length = len(content)
+        query = {
+            "query": {
+                "bool": {
+                    "must_not": {
+                        "exists": {
+                            "field": "length_type"
+                        }
+                    }
+                }
+            },
+            "_source": ["content"]
+        }
+        logger.info("[ES]length_type 미분류 기사 탐색 시작")
+        rows = helpers.scan(es, index=ES_INDEX, query=query)
+
+        count = 0
+        for row in rows:
+            news_id = row['_id']
+            content = row['_source'].get('content', '')
+
+            # 1. Length 분류
+            length_type = 'L' if len(content) >= 800 else 'S'
+
+            article_type = "Z"
+            info_type = "Z"
+            view_type = "Z"
+
+            combined_code = length_type + article_type + info_type + view_type
+
+            # 4. DB 객체 생성
+            length_type_list = ArticlesNPTI(
+                news_id=news_id,
+                length_type=length_type,
+                article_type=article_type,
+                info_type=info_type,
+                view_type=view_type,
+                NPTI_code=combined_code
+            )
+            # 존재하면 Update, 없으면 Insert
+            db.merge(length_type_list)
+            count += 1
+
+        db.commit()
+        logger.info(f"==========length_type {count}건 분류 및 DB 저장 완료==========")
+
     except Exception as e:
-        print(f'error 발생 또는 문서 없음 : {e}')
-        length = 0
-
-    if length >= 800:
-        length_type = 'long'
-    else :
-        length_type = 'short'
-    print(f'{news_id} -> {length_type}')
-    return length_type
+        logger.error(f"length_type 분류 및 DB 저장 처리 중 오류 발생-[classify_length()]: {e}")
+        db.rollback()
+    finally:
+        db.close()
 
 
-
-
+# 정답데이터로 머신러닝 학습
 # 2. article type 분류
 def classify_article(news_id:str):
     pass
-
 
 
 # 3. information type 분류
 def classify_information(news_id:str):
     pass
 
-############################################################# news_aggr 저장하는 함수 따로 만들기 (crawling 함수랑 분리)
 
 # 4. viewpoint type 분류
 def classify_viewpoint(news_id:str):
--- database.py
+++ database.py
@@ -2,25 +2,26 @@ from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
 from sqlalchemy.orm import sessionmaker, Session
 from logger import Logger
+from sqlalchemy.ext.declarative import declarative_base
 
 logger = Logger().get_logger(__name__)
 
-DB_HOST = "localhost"
-DB_PORT = 3306
-DB_NAME = "npti"
 DB_USER = "web_user"
 DB_PASSWORD = "pass"
+DB_NAME = "npti"
+DB_HOST = "localhost"
+DB_PORT = 3306
+DATABASE_URL = (f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
 
-DATABASE_URL = (
-    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+# ORM 모델 작성을 위한 기본 클래스
+Base = declarative_base()
 
-_engine: Engine | None = None
+_engine = None
+_SessionLocal = None
 
 
 def get_engine() -> Engine:
     global _engine
-
     if _engine is None:
         logger.info("DB Engine 생성")
         _engine = create_engine(
@@ -28,7 +29,6 @@ def get_engine() -> Engine:
             pool_pre_ping=True,
             pool_recycle=3600,
         )
-
     return _engine
 
 
@@ -37,7 +37,6 @@ SessionLocal = sessionmaker(
     autoflush=False,
     bind=get_engine(),
 )
-
 
 def get_db() -> Session:
     db = SessionLocal()
