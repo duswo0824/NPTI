1e513c62b7e2dd379fdad037fc9cdd16c31ae82a
한승연 <suzie011022@naver.com>
2026-01-06 14:53:59 +0900

    mappings, 유저 데이터 인덱싱 함수, 특정 유저의 행동 데이터 가져오는 함수 추가

--- elasticsearch_index/es_user_behavior.py
+++ elasticsearch_index/es_user_behavior.py
@@ -18,7 +18,18 @@ es = Elasticsearch( # elasticsearch 연결 객체 생성
 def ensure_index():
     body = {
         "mappings": {
-
+            "properties": {
+                "user_id": {"type": "keyword"},
+                "news_id": {"type": "keyword"},
+                "MMF_X_inf": {"type": "float"},
+                "MMF_Y_inf": {"type": "float"},
+                "MSF_Y_inf": {"type": "float"},
+                "mouseX": {"type": "float"},
+                "mouseY": {"type": "float"},
+                "n_word": {"type": "integer"},
+                "timestamp": {"type": "integer"},
+                "click": {"type": "integer"},
+            }
         }
     }
     if es.indices.exists(index=ES_INDEX):
@@ -35,3 +46,15 @@ def ensure_index():
             logger.error(f"index 생성 실패 : {res}")
     except Exception as e:
         logger.error(f"index 생성 오류 : {e}")
+
+def index_behavior_row(row:dict): # raw_news 데이터를 indexing하는 함수
+    es.index(index=ES_INDEX, id=row["news_id"], document=row, refresh="wait_for")
+
+def search_behavior(id_:str):
+    try:
+        result = es.exists(index=ES_INDEX, id=id_)  # ✅ exists() 사용 (더 빠름)
+        logger.info(f"ES 중복 확인 - {id_} : {'기존' if result else '신규'}")
+        return result
+    except Exception as e:
+        logger.error(f"ES 중복 확인 실패 {id_}: {e}")
+        return False
