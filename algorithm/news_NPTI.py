b011108f98d44fe76c3cbe8e521b4d30e951dae5
tannookim <tannookim@gmail.com>
2026-01-08 12:00:47 +0900

    warning ignore 추가 및 model 호출 방식 변경

--- algorithm/news_NPTI.py
+++ algorithm/news_NPTI.py
@@ -1,8 +1,6 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-import re
-
 import joblib
 from datetime import datetime, timezone, timedelta
 from logger import Logger
@@ -10,6 +8,7 @@ from elasticsearch import Elasticsearch, helpers
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.sql import func
 from database import Base, get_engine, SessionLocal
+import warnings
 
 logger = Logger().get_logger(__name__)
 
@@ -38,6 +37,12 @@ class ArticlesNPTI(Base):
     view_type = Column(String(10))  # P/N
     updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
 
+
+warnings.filterwarnings(
+    "ignore",
+    message="X does not have valid feature names"
+)
+
 # 테이블 생성 함수
 def add_db():
     engine = get_engine()
@@ -59,62 +64,38 @@ def err_article(news_id, error_message):
 # joblib 로드
 base_dir = os.path.dirname(os.path.abspath(__file__))
 model_dir = os.path.join(base_dir, "saved_models")
+_models = None
 def load_joblib():
-    logger.info("joblib 모델 및 벡터 로드 완료")
-    return {
-        "ct": (
-            joblib.load(os.path.join(model_dir, "model_ct.joblib")),
-            joblib.load(os.path.join(model_dir, "tfidf_ct.joblib")),
-        ),
-        "fi": (
-            joblib.load(os.path.join(model_dir, "model_fi.joblib")),
-            joblib.load(os.path.join(model_dir, "tfidf_fi.joblib")),
-        ),
-        "pn": (
-            joblib.load(os.path.join(model_dir, "model_pn.joblib")),
-            joblib.load(os.path.join(model_dir, "tfidf_pn.joblib")),
-        ),
-    }
+    global _models
+    if _models is None:
+        logger.info("joblib 모델 및 벡터 로드 시작")
+        _models = {
+            "ct": (
+                joblib.load(os.path.join(model_dir, "model_ct.joblib")),
+                joblib.load(os.path.join(model_dir, "tfidf_ct.joblib")),
+            ),
+            "fi": (
+                joblib.load(os.path.join(model_dir, "model_fi.joblib")),
+                joblib.load(os.path.join(model_dir, "tfidf_fi.joblib")),
+            ),
+            "pn": (
+                joblib.load(os.path.join(model_dir, "model_pn.joblib")),
+                joblib.load(os.path.join(model_dir, "tfidf_pn.joblib")),
+            ),
+        }
+        logger.info("joblib 모델 및 벡터 로드 완료")
+    return _models
 
-# F/I 분류용 사전 및 tokenizer
-FACTUAL_VERBS = {
-    '말하다','밝히다','전하다','설명하다','주장하다','전해지다'
-}
 
-FACT_PATTERNS = {
-    "에 따르면","전해졌다","밝혔다","확인됐다","발생했다","조사 결과","경찰은","검찰은","소방은","당국은","관계자는"
-}
+def init_npti():
+    logger.info("[NPTI INIT] DB 테이블 및 모델 초기화 시작")
+    add_db()
+    load_joblib()
+    logger.info("[NPTI INIT] 초기화 완료")
 
-INSIGHT_KEYWORDS = {
-    "의미","맥락","관점","해석","배경","평가",
-    "논란","쟁점","문제","시사점","함의",
-    "우려","비판","반박","옹호","핵심","본질","원인","영향","파장"
-}
-
-def tokenizer_fi(text: str):
-    tokens = re.findall(r"[가-힣]{2,}", text)
-    result = []
-
-    text_has_fact_pattern = any(p in text for p in FACT_PATTERNS)
-    for t in tokens:
-        # Insight 키워드는 유지
-        if t in INSIGHT_KEYWORDS:
-            result.append(t)
-        # Fact 패턴이 있는 기사라면
-        # FACTUAL_VERBS도 제거하지 않고 유지
-        elif text_has_fact_pattern:
-            result.append(t)
-        # Fact 패턴 없고, 서술 동사면 제거
-        elif t in FACTUAL_VERBS:
-            continue
-        else:
-            result.append(t)
-
-    return result
 
 # NPTI 라벨링 함수(joblib 모델 활용)
 def classify_npti_fast():
-    add_db()
     db = SessionLocal()
 
     models = load_joblib()
@@ -125,30 +106,33 @@ def classify_npti_fast():
     news_id = None
     try:
         now = datetime.now(timezone(timedelta(hours=9)))
-        since = now - timedelta(minutes=5)  # 5분 전 수집된 기사까지만 라벨링 # 이거랑 아래 쿼리문 주석 지워줘야됨
 
         query = {
             "query": {
-                "range": {
-                    "@timestamp": {
-                        "gte": since.isoformat()
-                    }
+                "term": {
+                    "classified": False
                 }
             },
             "_source": ["content"]
         }
-        existing_ids = set(row[0] for row in db.query(ArticlesNPTI.news_id).all())
 
         rows = helpers.scan(es, index=ES_INDEX, query=query)
         count = 0
 
         for row in rows:
             news_id = row["_id"]
-            if news_id in existing_ids:
-                continue
-
-            content = row["_source"].get("content", "")
+            content = row["_source"]["content"]
             if not content:
+                es.update(
+                    index=ES_INDEX,
+                    id=news_id,
+                    body={
+                        "doc": {
+                            "classified": True,
+                            "classified_reason": "empty_content"
+                        }
+                    }
+                )
                 continue
 
             try:
@@ -168,16 +152,36 @@ def classify_npti_fast():
                     NPTI_code=npti_code,
                     updated_at=now
                 )
-                db.merge(record)
+                db.add(record)
                 count += 1
+                db.commit()
+
+                es.update(
+                    index=ES_INDEX,
+                    id=news_id,
+                    body={
+                        "doc": {
+                            "classified": True,
+                            "npti": npti_code
+                        }
+                    }
+                )
 
             except Exception as e:
                 logger.error(f"[기사 분류 실패] news_id={news_id} / {e}")
                 err_article(news_id, e)
                 logger.info(f"기사 분류 실패 에러로그 저장 완료 - {news_id}")
+                es.update(
+                    index=ES_INDEX,
+                    id=news_id,
+                    body={
+                        "doc": {
+                            "classified": True,
+                            "classified_reason": "error"
+                        }
+                    }
+                )
                 continue
-
-        db.commit()
         logger.info(f"NPTI 신규 기사 {count}건 분류 완료")
 
     except Exception as e:
