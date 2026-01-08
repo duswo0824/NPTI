b693c686db06d330528143843085757644c17825
tannookim <tannookim@gmail.com>
2026-01-08 11:58:41 +0900

    Elasticsearch transport 로그 차단 추가

--- logger.py
+++ logger.py
@@ -6,6 +6,8 @@ class Logger:
         format='%(levelname)s : %(filename)s - %(asctime)s\n%(message)s',
         datefmt='%Y-%m-%d %H:%M:%S'
     )
+    # Elasticsearch transport 로그 차단
+    logging.getLogger("elastic_transport").setLevel(logging.WARNING)
 
     def get_logger(self,name):
         return logging.getLogger(name)
